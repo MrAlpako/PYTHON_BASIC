c4fcf7641a25ff9c6655518ee2b309cfeab55a03
Sergey <124809137+MrAlpako@users.noreply.github.com>
2024-10-19 16:25:24 +0300

    Add files via upload

---
+++ Classes__class_inheritance_.py
@@ -0,0 +1,46 @@
+﻿class Restoran():
+
+    def __init__(self, r_name, r_kuhnya):
+        self.r_name = r_name
+        self.r_kuhnya = r_kuhnya
+        self.num_gosti = 0
+        
+        
+    def inf_restoran(self):
+        print(f'Название ресторана: {self.r_name}\nКухня: {self.r_kuhnya}\n')
+        
+    def open_restoran(self):
+        print(f'Ресторан "{self.r_name}" открыт!\n_______________________\n')
+        
+    def update_gosti(self, num_gosti):
+        if num_gosti < 0:
+            print('Вы кого-то убили?')
+        else:
+            self.num_gosti += num_gosti
+                    
+    def gosti(self):
+        print(f'Гостей обсулжено: {self.num_gosti}\n...\n')     
+
+restoran = Restoran('Эшь', 'Вкусная')
+
+class Icecream(Restoran):
+    
+    def __init__(self, r_name, r_kuhnya):
+        super().__init__(r_name, r_kuhnya)
+        self.flavors = []
+        
+    def sorta(self, *sort):
+        self.sort = sort
+        self.flavors.append(self.sort)
+        print('Мороженное в наличии:')
+        for i in self.sort:
+            print('<0:',i)
+        
+ice=Icecream('Эшь', 'Вкусная')
+
+restoran.inf_restoran()
+restoran.open_restoran()
+restoran.update_gosti(-1)
+restoran.gosti()
+
+ice.sorta('Орео','Сникерс','Пломбир')
