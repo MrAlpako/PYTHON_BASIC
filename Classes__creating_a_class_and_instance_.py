b1c99b0d48038315e64d51ffcf2700f3e0346ec9
Sergey <124809137+MrAlpako@users.noreply.github.com>
2024-10-19 12:56:55 +0300

    Add files via upload

---
+++ Classes__creating_a_class_and_instance_.py
@@ -0,0 +1,20 @@
+﻿class Restoran():
+    
+    def __init__(self, r_name, r_kuhnya):
+        self.r_name = r_name
+        self.r_kuhnya = r_kuhnya
+        
+    def des_restoran(self):
+        print(f'Название ресторана: {self.r_name}\nКухня: {self.r_kuhnya}\n')
+        
+    def open_restoran(self):
+        print(f'Ресторан "{self.r_name}" открыт!\n')
+        
+restoran = Restoran('Эшь', 'Вкусная')
+restoran_2 = Restoran('НеЭшь', 'НеОчВкусная')
+
+restoran.des_restoran()
+restoran.open_restoran()
+
+restoran_2.des_restoran()
+restoran_2.open_restoran()
